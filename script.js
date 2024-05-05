eeb3c4fe008982f4d7606f203342b455ae475665
JM Heidrun <105260666+reiDoPerrengue@users.noreply.github.com>
2024-05-05 16:22:25 -0300

    v1

---
+++ script.js
@@ -0,0 +1,12 @@
+let fvalue = document.querySelector('input#fkm')
+let res = document.querySelector('div#res')
+
+function calcular() {
+    let value = Number(fvalue.value)
+    let taxa = 0.0449
+    let calculo = value * taxa
+    let calculoFinal = calculo + value
+
+    res.innerHTML = `<p> Precisamos cobrar do cliente o valor de ${calculoFinal.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})} para receber o valor acima. <\p>`
+}
+
